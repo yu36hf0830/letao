147c4451c098364fe23bf2a04c529b8bca3a8c5d
yulei <842282314@qq.com>
2017-11-30 05:57:45 +0800

    登录功能完成

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -13,7 +13,10 @@ $(function(){
                 validators:{
                     notEmpty:{
                         message:"用户不能为空"
-                    }
+                    },
+                    callback: {
+                        message:"用户名不存在"
+                      }
                 }
             },
             password:{
@@ -21,11 +24,15 @@ $(function(){
                     notEmpty:{
                         message:"密码不能为空"
                     },
+                   
                     stringLength:{
                         min:6,
                         max:12,
                         message:"密码长度为6-12位"
-                    }
+                    },
+                    callback: {
+                        message: "密码错误"
+                      }
                 }
             }
         }
@@ -39,16 +46,27 @@ $(function(){
             data: $form.serialize(),
             success:function(data){
                 if(data.success){
-                    location.href="index.html";
-                }
-                if(data.error==1000){
-                    alert("用户不存在");
-                }
-                if(data.error==1001){
-                    alert("密码错误");
-                }
+                    //跳转到首页
+                    location.href = "index.html";
+                  }
+                if(data.error == 1000){
+                    //alert("用户名不存在");
+                    //把用户名的校验失败
+                    //第一个参数：想要修改的字段
+                    //第二个参数：改成什么状态  INVALID  VALID
+                    //第三个参数： 指定显示的错误信息
+                    $form.data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                  }
+                  if(data.error == 1001){
+                    $form.data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+                  }
+          
+                
             }
         })
-    })
+    });
 
-})
+    $("[type='reset']").on("click",function(){
+        $form.data("bootstrapValidator").resetForm();
+    });
+});
